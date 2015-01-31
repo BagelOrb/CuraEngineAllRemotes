99890318d7f9664b17f1b2d5db79796e51ab380f
Bevaz <The.Bevaz@gmail.com>
2015-01-31 06:38:32 +0000

    Shippable.com build script for android

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-android-ndk-r10d/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk NDK_APPLICATION_MK=Application.mk V=0
+ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk NDK_APPLICATION_MK=Application.mk
 
